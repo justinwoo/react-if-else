356979f49e08379d6a4ac6f42fa7b0224615b9b7
Xianming Zhong <chinesedfan@qq.com>
2016-07-17 12:14:37 +0800

    handle the extra span for child text node, #4

--- modules/If.js
+++ modules/If.js
@@ -2,6 +2,18 @@ var React = require('react');
 var Else = require('./Else');
 var ElseIf = require('./ElseIf');
 
+/**
+ * Parse the version of React as an array of integers.
+ * Only contains the major and the minor version number.
+ */
+function parseVersion() {
+    var ver = React.version, matches;
+    if (matches = /^(\d+)\.(\d+)/.exec(ver)) {
+        return [parseInt(matches[1]), parseInt(matches[2])];
+    }
+    return [0, 0];
+}
+
 function isElseComponent(component) {
   return (component.type === Else.type || component.type === Else);
 }
@@ -58,6 +70,13 @@ var If = React.createClass({
     } else {
       output = this.props.children;
     }
+
+    // for React <15.0.0, keep child text node is wrapped by `span`, #4
+    if (parseVersion()[0] < 15 && output.children && output.children.length === 1
+            && typeof output.children[0] === 'string') {
+        output.children[0] = React.createElement('span', null, output.children[0]);
+    }
+
     var args = ["div", {className: output.className}].concat(output.children);
     return React.createElement.apply(this, args);
   }
